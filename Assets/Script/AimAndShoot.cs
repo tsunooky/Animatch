f92a77f0d2f7a06344f24ff02095e7eb86cf118b
Karma200IQ <josselin.priet@epita.fr>
2024-01-21 17:50:15 +0100

    Ajout de la gestion de projectile
    Creation d'un tube qui jette des asteroides a pleine vitesse et qui sont affecter par les collions mais ne creer pas d'explosion. manque de commmentaire et structure a changé des que possibles !

---
+++ Assets/Script/AimAndShoot.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AimAndShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+
+    private GameObject bulletInst;
+    
+    private Vector2 worldPosition;
+    private Vector2 direction;
+    
+    private void Start()
+    {
+        // Appeler la fonction GetMouseCoordinates() chaque seconde (1f seconde)
+        InvokeRepeating("Aim", 0f, 1f / 30f);
+    }
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    private void Aim()
+    {
+        // Récupérer les coordonnées de la souris en pixels
+        Vector2 mousePositionPixels = Mouse.current.position.ReadValue();
+
+        // Convertir les coordonnées de la souris de pixels à des coordonnées dans l'espace du monde
+        Vector2 mousePositionWorld = Camera.main.ScreenToWorldPoint(mousePositionPixels);
+        
+        direction = (mousePositionWorld - (Vector2)gameObject.transform.position).normalized;
+        gameObject.transform.right = direction;
+    }
+
+    private void Shoot()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            bulletInst = Instantiate(bullet, bulletSpawnPoint.position, gameObject.transform.rotation);
+        }
+    }
+    
+}
+
---
+++ Assets/Script/BulletBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 15f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SetStraightVelocity();
+    }
+
+    // Update is called once per frame
+    private void SetStraightVelocity()
+    {
+        rb.velocity = transform.right * speed;
+    }
+}

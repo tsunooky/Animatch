9e69640749eabf1ba2ea4e000d0ca6cc006e701b
Guilhem12345 <dardonville.guilhem@gmail.com>
2024-03-22 15:44:12 +0100

    Correction quelques bug lié à la caméra
    Nouveau bug trouvé : quand on bouge la caméra les  cartes si on passe dessus en restant cliquez sur clic droit bah elles font un peu n'imp, elles se mettent pas à leur bonne position et si jamais elles sortes du champ bah perdu à tout jamais mdrr(faut relancer la partie pour rénitialiser quoi)

--- UnityProject/Assets/Script/Autre/CameraMouv.cs
+++ UnityProject/Assets/Script/Autre/CameraMouv.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class CameraMouv : MonoBehaviour
 { 
-    public float dragSpeed = 0.2f;
+    public float dragSpeed = 12f;
     private Vector3 dragOrigin;
 
     // Définir les limites de la caméra
@@ -16,13 +16,13 @@ public class CameraMouv : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) 
         {
             dragOrigin = Input.mousePosition;
             return;
         }
 
-        if (!Input.GetMouseButton(1)) return;
+        if (!Input.GetMouseButton(1)) return; 
 
         Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
         Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
@@ -31,6 +31,8 @@ public class CameraMouv : MonoBehaviour
 
         // Limiter le mouvement de la caméra à la scène
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftLimit, rightLimit), Mathf.Clamp(transform.position.y, bottomLimit, topLimit),transform.position.z);
+
+        dragOrigin = Input.mousePosition; // Mettez à jour l'origine du glissement pour le prochain frame
     }
 }
 

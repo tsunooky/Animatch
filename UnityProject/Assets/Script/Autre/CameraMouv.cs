9c6682b620425247d189491b318bb22c33a3da11
Guilhem12345 <dardonville.guilhem@gmail.com>
2024-03-22 12:41:44 +0100

    Ajout de la camera qui peut bouger
    J'ai ajouter le mouvement de la caméra, c'est à dire que si vous voulez bouger sur l'écran faut se déplacer  avec le clique droit pour l'instant et voila a peu près

---
+++ UnityProject/Assets/Script/Autre/CameraMouv.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMouv : MonoBehaviour
+{ 
+    public float dragSpeed = 0.2f;
+    private Vector3 dragOrigin;
+
+    // Définir les limites de la caméra
+    public float leftLimit = -10.0f;
+    public float rightLimit = 10.0f;
+    public float topLimit = 6.0f;
+    public float bottomLimit = -5.0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(1)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
+        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+
+        transform.Translate(move, Space.World);
+
+        // Limiter le mouvement de la caméra à la scène
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftLimit, rightLimit), Mathf.Clamp(transform.position.y, bottomLimit, topLimit),transform.position.z);
+    }
+}
+
+

00f33e0d65937fd0ad82c87cba8092eb3620b916
Adrien Coureau <adrien.coureau@epita.fr>
2024-04-23 14:49:41 +0200

    Tomate fonctionne a nouveau mais plus sur la map

--- UnityProject/Assets/Script/Projectile/Behaviour/ProjTomateBehaviour.cs
+++ UnityProject/Assets/Script/Projectile/Behaviour/ProjTomateBehaviour.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Destructible2D.Examples;
+using Script.Manager;
 
 public class ProjTomateBehaviour : ProjectileBehaviour
 {
-    /*private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.gameObject.name);
-        if (!declanchement && other.gameObject != projectileData.Lanceur)
-        { 
-            declanchement = true;
-            var clone = Instantiate(Prefab, transform.position, transform.rotation);
-            clone.SetActive(true);
-            clone.GetComponent<D2dExplosion>().degat = projectileData.Degat;
-            FinAction();
-            Destroy(gameObject);
+        GameObject cible = other.gameObject.transform.parent.gameObject;
+        GameObject AnimalActif = GameManager.Instance.playerActif.animalActif.gameObject;
+        bool invinsible = cible == AnimalActif; 
+        if (!invinsible)
+        {
+            if (!declanchement && other.gameObject != projectileData.Lanceur)
+            {
+                declanchement = true;
+                var clone = Instantiate(Prefab, transform.position, transform.rotation);
+                clone.SetActive(true);
+                clone.GetComponent<D2dExplosion>().degat = projectileData.Degat;
+                FinAction();
+                Destroy(gameObject);
+            }
         }
-    }*/
+    }
 }

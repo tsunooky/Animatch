f65627b22d1247c59c2d0ebc1c95e00fe56ba1c4
hecthor-html <78790904+hecthor-html@users.noreply.github.com>
2024-06-11 15:12:58 +0200

    update aimdata

--- UnityProject/Assets/Script/Projectile/Behaviour/AimBot.cs
+++ UnityProject/Assets/Script/Projectile/Behaviour/AimBot.cs
@@ -7,7 +7,7 @@ public  class aimBot : MonoBehaviour
 {
     public void ClassiqueShootbot(Vector2 targetPosition)
     {
-        var pro = Resources.Load<ProjectileData>("Data/Carte/Tomate");
+        var pro = Resources.Load<ProjectileData>("Data/Projectile/Tomate");
         GameObject bullet = Instantiate(pro.Projectile, gameObject.transform.position, Quaternion.identity);
         ProjectileBehaviour bulletBehaviour = bullet.GetComponent<ProjectileBehaviour>();
         bulletBehaviour.Set((gameObject.transform.position,targetPosition), pro);

5f2048a720f7a7ffd04a051be2d260641a1b5c13
Adrien Coureau <adrien.coureau@epita.fr>
2024-06-11 17:04:05 +0200

    Debut du menu et des sons/musique
    - Ajout d'une nouvelle scene menu
    - Ajout du dessin du menu
    - Ajout d'un materiau + shader pour un effet de vent
    - Création d'un bouton du menu + animation (pas termine j'ai un problème bizarre mon rider fonctionne plus donc je vais continuer juste apres mais touchez pas a la scene pour l'instant svp)
    - Création d'un script LibraryManager pour gérer les sons
    - Creation d'un script SoundManager pour gerer l'envoi de sons a l'utilisateur

---
+++ UnityProject/Assets/Script/Manager/SoundLibrary.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Resources.Sounds
+{
+    [System.Serializable]
+    public struct SoundEffect
+    {
+        public string groupID;
+        public AudioClip[] clips;
+    }
+ 
+    public class SoundLibrary : MonoBehaviour
+    {
+        public SoundEffect[] soundEffects;
+ 
+        public AudioClip GetClipFromName(string name)
+        {
+            foreach (var soundEffect in soundEffects)
+            {
+                if (soundEffect.groupID == name)
+                {
+                    return soundEffect.clips[Random.Range(0, soundEffect.clips.Length)];
+                }
+            }
+            return null;
+        }
+    }
+}

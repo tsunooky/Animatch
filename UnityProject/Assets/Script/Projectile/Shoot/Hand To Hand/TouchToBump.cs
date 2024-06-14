470353c82c4d0bd8e6f18122ebd143a250877560
Adrien Coureau <adrien.coureau@epita.fr>
2024-06-14 15:50:38 +0200

    bug panda résolu et bat presque finis
    IAAAAAAAAAAAAAAAAA

--- UnityProject/Assets/Script/Projectile/Shoot/Hand To Hand/TouchToBump.cs
+++ UnityProject/Assets/Script/Projectile/Shoot/Hand To Hand/TouchToBump.cs
@@ -7,7 +7,6 @@ public class TouchToBump : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Joss est vraiment un GIGA GOAT");
         GameObject cible = other.gameObject.transform.parent.gameObject;
         GameObject AnimalActif = GameManager.Instance.playerActif.animalActif.gameObject;
         bool invinsible = cible == AnimalActif;
@@ -18,12 +17,11 @@ public class TouchToBump : MonoBehaviour
 
             // Coordonnées de la cible
             Vector2 ciblePosition = cible.transform.position;
-
             // Direction de rejet (de la cible vers l'animal actif, inversée)
-            Vector2 bumpDirection = (ciblePosition - startPosition).normalized * -1;
+            Vector2 bumpDirection = (ciblePosition - startPosition);
 
             // Appliquer la force de rejet à la cible
-            float bumpForce = 50; // Ajuster cette valeur si nécessaire
+            float bumpForce = 160000; // Ajuster cette valeur si nécessaire
             cible.GetComponent<Rigidbody2D>().AddForce(bumpDirection * bumpForce);
         }
     }

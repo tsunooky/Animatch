cfa4c29c9d802c497417d82ba17f9a226c04016f
Adrien Coureau <adrien.coureau@epita.fr>
2024-06-15 19:46:36 +0200

    Ajout bouton règles qui mène au site internet

---
+++ UnityProject/Assets/Script/Manager/UI/RuleButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleButton : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://animatch.site/");
+    }
+}
+

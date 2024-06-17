ae737cd6226a5767f3412ebcbc657ffa174ef362
Adrien Coureau <adrien.coureau@epita.fr>
2024-06-17 19:44:50 +0200

    La bombe tombe d'un peu plus haut

--- UnityProject/Assets/Script/Projectile/Proj_Behaviour/ProjBombeBehaviour.cs
+++ UnityProject/Assets/Script/Projectile/Proj_Behaviour/ProjBombeBehaviour.cs
@@ -20,7 +20,7 @@ public class ProjBombeBehaviour : ProjectileBehaviour
         if (other.gameObject.transform.parent is not null)
         {
             GameObject cible = other.gameObject.transform.parent.gameObject;
-            GameObject AnimalActif = GameManager.Instance.playerActif.animalActif.gameObject;
+            GameObject AnimalActif = AGameManager.Instance.playerActif.animalActif.gameObject;
             bool invinsible = cible == AnimalActif;
             if (!invinsible)
             {
@@ -28,7 +28,7 @@ public class ProjBombeBehaviour : ProjectileBehaviour
                 {
                     declanchement = true;
                     var startPosition = gameObject.transform.position;
-                    startPosition.y += 5;
+                    startPosition.y += 8;
                     GameObject atomique = Instantiate(Atomique, startPosition, Quaternion.Inverse(quaternion.identity));
                     atomique.GetComponent<ProjectileBehaviour>().Prefab = projectileData.Explosion;
                     atomique.GetComponent<ProjectileBehaviour>().projectileData = projectileData;

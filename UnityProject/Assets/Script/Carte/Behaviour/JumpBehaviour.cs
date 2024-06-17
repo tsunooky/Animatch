7a93dbb3673e209a729ff078dbab17efbc561c71
hecthor-html <78790904+hecthor-html@users.noreply.github.com>
2024-06-17 17:08:12 +0200

    neuille

--- UnityProject/Assets/Script/Carte/Behaviour/JumpBehaviour.cs
+++ UnityProject/Assets/Script/Carte/Behaviour/JumpBehaviour.cs
@@ -9,7 +9,7 @@ public class JumpBehaviour : CarteBehaviour
     protected override void Awake()
     {
         // TEMPORAIRE OU DEFINITIF EN FONCTION (Bump)
-        Bump = 15;
+        Bump = 10;
         carteData = Resources.Load<CarteData>("Data/Carte/Jump");
     }
 
